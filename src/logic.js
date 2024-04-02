d8fc1ec64da615f344e8a65acec6e132a8c308a1
nk-on <2nkoz@proton.me>
2024-04-02 22:13:26 +0400

    Feat:implementing task creation feature

--- src/logic.js
+++ src/logic.js
@@ -1,4 +1,6 @@
 const projectInput = document.querySelector('#project-description');
+const taskDescription = document.querySelector('#task-description');
+const taskDueDate = document.querySelector('#date');
 class Project {
   constructor(title, id) {
     this.title = title;
@@ -6,8 +8,19 @@ class Project {
     this.tasks = [];
   }
 }
+class Task {
+  constructor(title, dueDate, id) {
+    this.title = title;
+    this.dueDate = dueDate;
+    this.id = id;
+  }
+}
+function createTask() {
+  const taskInstance = new Task(taskDescription.value, taskDueDate.value, Date.now());
+  return taskInstance;
+}
 function createProject() {
   const projectInstance = new Project(projectInput.value, Date.now());
   return projectInstance;
 }
-export  { createProject };
+export { createProject, createTask };
--- src/taskCreation.js
+++ src/taskCreation.js
@@ -1,4 +1,41 @@
-import { projectArray, selectedProject, saveProject } from './main.js';
-const addTaskForm = document.querySelector('[task-dialog]');
-const taskDescription = document.querySelector('#task-description');
-console.log(projectArray)
+import {
+  projectArray,
+  selectedProject,
+  saveProject,
+} from './projectCreation.js';
+import { createTask } from './logic.js';
+const addTaskForm = document.querySelector('[data-task-dialog]');
+const taskTemplate = document.querySelector('#task');
+const taskContainer = document.querySelector('[data-task-container]');
+const taskArray = [];
+function renderTaskArray(tasks) {
+  if (taskContainer.innerHTML.length) {
+    taskContainer.innerHTML = '';
+  }
+  tasks.forEach((task) => {
+    const taskElement = document.importNode(taskTemplate.content, true);
+    const taskDiv = taskElement.querySelector('#task')
+    const taskTitle = taskElement.querySelector('[data-task-title]');
+    const taskDueDate = taskElement.querySelector('[data-task-dueDate]');
+    taskTitle.textContent = task.title;
+    taskDueDate.textContent = task.dueDate;
+    taskContainer.appendChild(taskElement);
+    taskDiv.classList.add(String(task.id));
+  });
+}
+function createTaskArray() {
+  //if selected project is empty add to general taskArray
+  //else render from project subtasks
+  const task = createTask();
+  if (selectedProject) {
+    selectedProject.tasks.push(task);
+    renderTaskArray(selectedProject.tasks);
+    return;
+  }
+  taskArray.push(task);
+  renderTaskArray(taskArray);
+}
+addTaskForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  createTaskArray();
+});
